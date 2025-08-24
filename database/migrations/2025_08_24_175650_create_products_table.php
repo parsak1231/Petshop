069c4f6bc25a808f0cfee17891f25ca6bc78f219
parsak1321 <parsak1321@gmail.com>
2025-08-24 18:01:11 +0330

    pet_shop

--- database/migrations/2025_08_24_175650_create_products_table.php
+++ database/migrations/2025_08_24_175650_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
 
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained();
+            $table->foreignId('category_id')->constrained('categories');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->text('image');
             $table->bigInteger('price');
